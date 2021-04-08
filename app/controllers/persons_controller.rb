b5c8c30a431e287d8e2df6794ea6063663849f51
Adeel <adeel@devsinc.com>
2021-04-08 22:35:16 +0500

    Query Interface Practice

--- app/controllers/persons_controller.rb
+++ app/controllers/persons_controller.rb
@@ -11,12 +11,62 @@ class PersonsController < ApplicationController
   def index
     #p session[:person]
     #p flash[:person]
+    # p Person.present?
+    # p Person.exists?(2)
+    # p Person.pluck
+    # p Person.select{ |person| true }
+    # p Person.find(1)
+    # p Person.find([1,3])
+    # p Person.find(1,3)
+    # p Person.find(10)
+    # p Person.take
+    # p Person.take(10)
+    # p Person.take!(10)
+    # p Person.where(id: 1..5)
+    # p Person.first
+    # p Person.first(10)
+    # p Person.order(:name).first
+    # p Person.last
+    # p Person.last(10)
+    # p Person.order(:name).last
+    # p Person.find_by(name: 'Adeel')
+    # p Person.find_by!(name: 'Adeel')
+    # p Person.where(name: 'Adeel').take
+    # Person.find_each do |person|
+    #   p person
+    # end
+
+    # Person.where(name: 'Adeel').find_each do |person|
+    #   p person
+    # end
+
+    # Person.find_each(batch_size: 2, start: 2, finish: 5) do |person|
+    #   p person
+    # end
+
+    # Person.find_in_batches do |person|
+    #   p person
+    # end
+
+    # Person.find_in_batches(batch_size: 2, start: 2, finish: 5) do |person|
+    #   p person
+    # end
+
+    # p Person.where('name = ?', 'AdeelShabbir')
+    # p Person.where(name: 'AdeelShabbir')
+    # p Person.where('name = :name', {name: 'AdeelShabbir'})
+    # p Person.where(id: [1,3,5])
+    # p Person.where.not(name: 'Adeel')
+    # p Person.where.not(name: 'Adeel').or(Person.where(id: [1,3,5]))
+    # p Person.order(:name)
+    # p Person.order(id: :desc)
+    # p Person.order(id: :desc, name: :asc)
     @persons = Person.all
+    #puts @person.present?
   end
 
   def create
     @person = Person.new(person_params)
-
     if @person.save
       PersonMailer.with(person: @person).person_signed_up.deliver_later
       redirect_to @person
